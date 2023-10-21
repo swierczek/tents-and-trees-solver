56203b90eec6ce457ac77f833db5fdb7fbbc8ef6
Eric Swierczek <ericswierczek@gmail.com>
2023-10-20 23:54:13 -0500

    Display cropped color image

--- public/js/main.js
+++ public/js/main.js
@@ -15,31 +15,15 @@ var onOpenCvReady = function() {
     document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
 
     let src = cv.imread(imgElement);
-    let dst = new cv.Mat();
+    // let dst = new cv.Mat();
 
     // imageMetadata(src);
 
     src = cropGrid(src);
 
-    // let cnt = contours.get(0);
-    // You can try more different parameters
-    // let rect2 = cv.boundingRect(cnt);
-    // let contoursColor = new cv.Scalar(255, 255, 255);
-    // let rectangleColor = new cv.Scalar(255, 0, 0);
-    // cv.drawContours(dst, contours, 0, contoursColor, 1, 8, hierarchy, 100);
-    // let point1 = new cv.Point(rect2.x, rect2.y);
-    // let point2 = new cv.Point(rect2.x + rect2.width, rect2.y + rect2.height);
-    // cv.rectangle(dst, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
-
-    // draw contours with random Scalar
-    // for (let i = 0; i < contours.size(); ++i) {
-    //     let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
-    //                               Math.round(Math.random() * 255));
-    //     cv.drawContours(edges, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-    // }
-
     cv.imshow('canvasOutput', src);
-    src.delete(); dst.delete();
+    // dst.delete();
+    src.delete();
 }
 
 function imageMetadata(src) {
@@ -52,27 +36,17 @@ function imageMetadata(src) {
 }
 
 /**
- * remove the top 15% of the image
+ * Crop out the background of the image
+ *
  * @param src full color image
- * @return cropped full color image
+ * @return src full color image cropped
  */
-function removeTop(src) {
-    // x, y, width, height
-    let rect = new cv.Rect(
-        0,
-        (src.size().height / 100) * 15,
-        src.size().width,
-        (src.size().height / 100) * 85
-    );
-    return src.roi(rect);
-}
-
 function cropGrid(src) {
     // remove the top 15%
     src = removeTop(src);
 
     // to find contours, background should be black
-    bw = blackAndWhite(src);
+    let bw = blackAndWhite(src);
 
     // start both in the middle and work up/down from there
     let topCrop = bw.size().height / 2;
@@ -97,9 +71,6 @@ function cropGrid(src) {
         }
     }
 
-    console.log('top', topCrop);
-    console.log('bottom', bottomCrop);
-
     let rect = new cv.Rect(
         0,
         topCrop - 3,
@@ -114,15 +85,39 @@ function cropGrid(src) {
     return src.roi(rect);
 }
 
+/**
+ * remove the top 15% of the image
+ * @param src full color image
+ * @return cropped full color image
+ */
+function removeTop(src) {
+    // x, y, width, height
+    let rect = new cv.Rect(
+        0,
+        (src.size().height / 100) * 15,
+        src.size().width,
+        (src.size().height / 100) * 85
+    );
+    return src.roi(rect);
+}
+
+/**
+ * Convert the src to black and white
+ *
+ * @param src full color image
+ * @return binary black/white image
+ */
 function blackAndWhite(src) {
+    let bw = new cv.Mat();
+
     // grayscale it for easier detection
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.cvtColor(src, bw, cv.COLOR_RGBA2GRAY, 0);
 
     // https://docs.opencv.org/3.4/d7/dd0/tutorial_js_thresholding.html
     // figure out which funtion and params
-    cv.threshold(src, src, 50, 255, cv.THRESH_BINARY);
+    cv.threshold(bw, bw, 50, 255, cv.THRESH_BINARY);
 
-    return src;
+    return bw
 }
 
 // blur to help with edge detection and combat image compression?
